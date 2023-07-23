3433a46a7217ac38a34aecd62d5f93c911d2b44d
ykob <info@tplh.net>
2023-07-23 23:18:58 +0900

    update the card component.

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,21 +1,38 @@
 <script setup lang="ts">
 import { computed } from "vue";
+import { zeroPadding } from "../utils";
 
 const props = defineProps<{
+  gottenItemIds: number[];
+  imageId: number;
   itemIndex: number;
-  memberId: number;
   selectedItemIndices: number[];
 }>();
 const classnames = computed(() => {
   return {
-    "is-flipped": props.selectedItemIndices.includes(props.itemIndex),
-  }
+    "is-flipped": isFlipped.value,
+  };
+});
+const isFlipped = computed(() => {
+  return (
+    props.gottenItemIds.includes(props.imageId) ||
+    props.selectedItemIndices.includes(props.itemIndex)
+  );
+});
+const src = computed(() => {
+  return `/img/card${zeroPadding(props.imageId, 2)}.jpg`;
 });
 </script>
 
 <template>
   <div class="card-item">
-    <div class="card-item-front" :class="classnames"></div>
-    <div class="card-item-back" :class="classnames">{{ props.memberId }}</div>
+    <div class="card-item-front" :class="classnames">
+      <img v-if="isFlipped" :src="src" alt="" />
+    </div>
+    <div class="card-item-back" :class="classnames">
+      <img src="/img/card_back.jpg" alt="" />
+    </div>
   </div>
 </template>
+
+<style scoped></style>
