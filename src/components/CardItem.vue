fc5c6467e18806ed6e1092cdd55b848ec63398ef
ykob <info@tplh.net>
2023-07-24 22:12:18 +0900

    added styles for the card item component.

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -26,13 +26,53 @@ const src = computed(() => {
 
 <template>
   <div class="card-item">
-    <div class="card-item-front" :class="classnames">
+    <div class="front" :class="classnames">
       <img v-if="isFlipped" :src="src" alt="" />
     </div>
-    <div class="card-item-back" :class="classnames">
+    <div class="back" :class="classnames">
       <img src="/img/card_back.jpg" alt="" />
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-item {
+  cursor: pointer;
+  width: 120px;
+  height: 180px;
+  color: #fff;
+  transform-style: preserve-3d;
+  perspective: 640px;
+}
+.card-item img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.front,
+.back {
+  overflow: hidden;
+  border-radius: 8px;
+  transition-property: transform;
+  transition-duration: 0.2s;
+}
+.front {
+  position: absolute;
+  inset: 0;
+  transform: rotateY(180deg);
+  backface-visibility: hidden;
+}
+.front.is-flipped {
+  transform: rotateY(0deg);
+}
+.back {
+  position: absolute;
+  inset: 0;
+  transform: rotateY(0deg);
+  backface-visibility: hidden;
+}
+.back.is-flipped {
+  transform: rotateY(180deg);
+}
+
+</style>
