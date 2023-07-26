116ca6a371a30b90d4394fcd0e87028edeadf32f
ykob <info@tplh.net>
2023-07-26 23:26:45 +0900

    added the game timer component.

---
+++ src/components/GameTimer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { getTime } from "../../utils"
+
+const props = defineProps<{
+  timeCurrent: number;
+  timeStart: number;
+}>();
+const timeValue = computed(() => {
+  return getTime(props.timeCurrent - props.timeStart);
+});
+</script>
+
+<template>
+  <div class="game-timer">
+    {{ timeValue }}
+  </div>
+</template>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 import CardItem from "./CardItem.vue";
 import CardItemContainer from "./CardItemContainer.vue";
 import GameContent from "./GameContent.vue";
+import GameTimer from "./GameTimer.vue";
 export {
   CardItem,
   CardItemContainer,
   GameContent,
+  GameTimer,
 };
