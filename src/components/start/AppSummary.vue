2d32d4d35db7efff45d3db1a4e20139777533f8c
ykob <info@tplh.net>
2023-07-30 19:03:56 +0900

    adjusted the app summary styles.

--- src/components/start/AppSummary.vue
+++ src/components/start/AppSummary.vue
@@ -8,8 +8,20 @@
 </template>
 
 <style scoped>
+.app-summary {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+}
 h1 {
   margin: 0;
   line-height: 1;
+  font-size: 4em;
+  letter-spacing: 0.02em;
+}
+p {
+  font-size: 1.2rem;
+  letter-spacing: 0.06em;
 }
 </style>
