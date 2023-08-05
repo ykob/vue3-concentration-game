36f580602d60d69d629801e8f870bc9ffa12609f
ykob <info@tplh.net>
2023-08-05 15:20:56 +0900

    added refresh icon on retry button.

--- src/components/result/RetryButton.vue
+++ src/components/result/RetryButton.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { FilledButton } from "../common";
+import { mdiRefresh } from "@mdi/js";
+import { FilledButton, IconBase } from "../common";
 
 const emits = defineEmits(["retryGame"]);
 </script>
 
 <template>
   <FilledButton size="lg" @click="emits('retryGame')">
+    <IconBase :path="mdiRefresh" />
     Retry
   </FilledButton>
 </template>
