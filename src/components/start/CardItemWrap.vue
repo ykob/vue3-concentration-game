76480eb1d66a6e3586cd03b7072f58340cfa2466
ykob <info@tplh.net>
2023-08-07 00:19:58 +0900

    added loop animation for card on the start content.

--- src/components/start/CardItemWrap.vue
+++ src/components/start/CardItemWrap.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { onMounted, reactive } from "vue";
 import { CardItem } from "../common";
+import { CARD_COUNT } from "../../constants";
 
 const params = reactive<{
   imageId: number;
@@ -9,11 +10,26 @@ const params = reactive<{
   imageId: 0,
   isFlipped: false,
 });
+
+const update = () => {
+  if (!params.isFlipped)
+    params.imageId = Math.floor(Math.random() * CARD_COUNT);
+  params.isFlipped = !params.isFlipped;
+  setTimeout(update, 2000);
+};
+
+onMounted(() => {
+  setTimeout(update, 2000);
+});
 </script>
 
 <template>
   <div class="card-item-wrap">
-    <CardItem :image-id="params.imageId" :is-flipped="params.isFlipped" />
+    <CardItem
+      :image-id="params.imageId"
+      :is-flipped="params.isFlipped"
+      disabled
+    />
   </div>
 </template>
 
